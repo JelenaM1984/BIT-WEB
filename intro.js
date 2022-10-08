2da3b9b0c9dc32390e26939b7d93ca8d9c43871c
Jelena Milovanovic <milovanovicjelena84@gmail.com>
2022-10-08 16:05:06 +0200

    _Naziv

---
+++ intro.js
@@ -0,0 +1,35 @@
+// var a;
+// a=60*125
+// console.log(a);
+// var b
+// b=1024*24
+// console.log(b);
+// console.log(1=='1');
+// console.log(1==='1');
+// console.log(1!=='1');
+// console.log(1>='1');
+// var a;
+// console.log(a);
+// var b;
+// b=!!a;
+// console.log(b);
+// var n3=0377;
+// console.log (typeof n3);
+// console.log(n3);
+// var result="", a=2, b=5;
+// if (a>b) {
+//     result='a is greater than b'
+// } else if (a<b) {
+//     result='a is less than b'
+// }
+// else {
+//     result='a is equal to b'
+// }
+// console.log(result);
+
+    
+} else {
+    
+}
+
+
