0fe6a0127df593ae1bd400e8f156ff4994b12530
Jelena Milovanovic <milovanovicjelena84@gmail.com>
2022-10-15 14:06:22 +0200

    Swich

--- swich.js
+++ swich.js
@@ -1,26 +1,117 @@
-var a = 1;
+// var a = 10;
+// switch (a) {
+//     case 1:
+//         result = 'ponedeljak';
+//         break;
+//     case 2:
+//         result = 'utorak';
+//         break;
+//     case 3:
+//         result = 'sreda';
+//         break;
+//     case 4:
+//         result = 'cetvrtak';
+//         break;
+//     case 5:
+//         result = 'petak';
+//         break;
+//     case 6:
+//         result = 'subota';
+//         break;
+//     case 7:
+//         result = 'nedelja';
+//         break;
+//         default:
+//             result='input must be a number between 1 and 7';
+// }
+// console.log(result);
+// var a = 3;
+// switch (a) {
+//     case 1:
+//         result = 'it/s a weekday';
+//         break;
+//     case 2:
+//         result = 'it/s a weekday';
+//         break;
+//     case 3:
+//         result = 'it/s a weekday';
+//         break;
+//     case 4:
+//         result = 'it/s a weekday';
+//         break;
+//     case 5:
+//         result = 'it/s a weekday';
+//         break;
+//     case 6:
+//         result = 'It/s a weekend';
+//         break;
+//     case 7:
+//         result = 'It/s a weekend';
+//         break;
+//     default:
+//         result = 'input must be a number between 1 and 7';
+// }
+// console.log(result);
+// var b = 3;
+// switch (b) {
+//     case 1:
+//         result = 'Januar';
+//         break;
+//     case 2:
+//         result='Februar';
+//         break;
+//     case 3:
+//         result='Mart';
+//         break;
+//     case 4:
+//         result='April';
+//         break;
+//     case 5:
+//         result='Maj';
+//         break;
+//     case 6:
+//         result='Jun';
+//         break;
+//     case 7:
+//         result='Jul';
+//         break;
+//     case 8:
+//         result='Avgust';
+//         break;
+//     case 9:
+//         result='Septembar';
+//         break;
+//     case 10:
+//         result='Oktobar';
+//         break;
+//     case 11:
+//         result='Novembar';
+//         break;
+//     case 12:
+//         result='Decembar';
+//         break;
+//     default: 'Input must be a number between 1 and 12';
+//         break;
+// }
+// console.log(result);
+var a = 'A';
 switch (a) {
-    case 1:
-        result = 'ponedeljak'
+    case 'A':
+        result = 'Good job';
         break;
-    case 2:
-        result = 'utorak'
+    case 'B':
+        result='Pretty good';
         break;
-    case 3:
-        result = 'sreda'
+    case 'C':
+        result='Past';
         break;
-    case 4:
-        result = 'cetvrtak'
+    case 'D':
+        result='Not so good';
         break;
-    case 5:
-        result = 'petak'
+    case 'F':
+        result='Failed';
         break;
-    case 6:
-        result = 'subota'
-        break
-    case 7:
-        result = 'nedelja'
-        break
-        default:'input must be a number between 1 and 7'
+     default: 'Unknown grade';
+        break;
 }
 console.log(result);
