5475b937c0bb499d68813d0f3f1a053dd67411b0
Jelena Milovanovic <milovanovicjelena84@gmail.com>
2022-10-29 14:20:51 +0200

    novo

---
+++ INTRO/loops1.js
@@ -0,0 +1,12 @@
+var a = [5, -4.2, 9, 7];
+var e = 3;
+var result = "";
+for (i = 0; i < a.length; i++) {
+  if (a[i] == e) {
+    result = "yes";
+  } else {
+    result = "no";
+  }
+}
+
+console.log(result);
