7ece0d430c56d721c684a83d678c10fabd410ad0
Jelena Milovanovic <milovanovicjelena84@gmail.com>
2023-02-01 17:50:31 +0100

    ajax

--- Radno/JavaScript/ajax.js
+++ Radno/JavaScript/ajax.js
@@ -1,10 +1,19 @@
-function getDogImage(){
-    var request=$.ajax({
-        url:"https://dog.ceo/dog-api/",
-        method:"GET"
+function getDogImage() {
+  var request = $.ajax({
+    url: "https://dog.ceo/api/breeds/image/random",
+    method: "GET",
+  });
+  request
+    .done(function (response) {
+      console.log(response);
+      var img = $("<img/>").attr("src", response.message);
+      $("#getImage").html("");
+      $("#getImage").append(img);
     })
-    request.done(function(response){
-        console.log(response);
-        var img=$('<img/>').attr('src',response.message)
+    .fail(function (response) {
+      console.log(response)
     })
+    .always(function (response) {
+      console.log("zahtev zavrsen")
+    });
 }
