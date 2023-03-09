649432f7980a47903685807ea56b9554f1dfc3cc
Jelena Milovanovic <milovanovicjelena84@gmail.com>
2023-03-09 17:00:44 +0100

    blog

---
+++ REACT/react-blog/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Post from "./Post/post";
+import Header from "./Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Post />
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/react-blog/src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./header.scss";
+
+
+export default function Header(){
+    return (
+        <div className="container">
+        <h1>React Blog</h1>
+        </div>
+    );
+}
